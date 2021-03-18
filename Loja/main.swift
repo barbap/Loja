fe0bb1668db26f0689a393112f9d609d0a3458d5
barbap <barbaraaraujopaim99@gmail.com>
2021-03-18 10:48:00 -0300

    Classes "Produto" e "Usuario" foram criadas

--- Loja/main.swift
+++ Loja/main.swift
@@ -7,5 +7,31 @@
 
 import Foundation
 
-print("Hello, Bárbara!")
-
+class Usuario {
+    let codigo: Int
+    let nome: String
+    let email: String
+    let senha: String
+    
+    init(codigo: Int, nome: String, email: String, senha: String) {
+        self.codigo = codigo
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+    }
+}
+class Produto {
+    let codigo: Int
+    let nome: String
+    let descricao: String
+    let valor: Double
+    let data_publicacao: Date
+    
+    init(codigo: Int,nome: String,descricao: String,valor: Double,data_publicacao: Date) {
+        self.codigo = codigo
+        self.nome = nome
+        self.descricao = descricao
+        self.valor = valor
+        self.data_publicacao = data_publicacao
+    }
+}
